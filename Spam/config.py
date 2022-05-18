0adaa12c063b3ba2828264afc4405122eb5e10a8
MataFort <99809557+MataFort@users.noreply.github.com>
2022-05-18 16:05:28 +0300

    Add files via upload

---
+++ Spam/config.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "5367200591:AAH701sHcgF33Gt-P1mc470INwohrgbnTvQ" # ТОКЕН БОТА
+ADMIN = 5070762723 # ИД АДМИНА
+
+DIR = "" 
+
+# https://my.telegram.org/auth
+API_ID = 19787011
+API_HASH = "4b9d091e6da612df9eec836626e9041a"
